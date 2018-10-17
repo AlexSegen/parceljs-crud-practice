e21eeb51fc92635d658d5211d15d4ac90a706f5f
Alejandro Vivas <alejandrojv17@gmail.com>
2018-10-17 17:35:06 -0300

    NOne

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import _ from "underscore";
 import userServices from "./user.services";
 
+class User {
+  constructor() {
+    this.id = "";
+    this.guid = "";
+    this.isActive = false;
+    this.balance = "$0";
+    this.picture = "http://placehold.it/32x32";
+    this.age = 18;
+    this.first_name = "";
+    this.last_name = "";
+    this.company = "";
+    this.email = "";
+    this.phone = "";
+    this.address = "";
+    this.about = "";
+    this.registered = "";
+    this.greeting = "";
+    this.favoriteFruit = "strawberry";
+  }
+}
+
 const printText = msg => {
   document.getElementById("app").innerHTML = `<h1>${msg}</h1>`;
 };
@@ -179,5 +200,4 @@ document.addEventListener(
 );
 
 listUsers();
-
 printText("Users");
---
+++ src/user.class.js
@@ -0,0 +1,20 @@
+class User {
+  constructor() {
+    this.id = "";
+    this.guid = "";
+    this.isActive = false;
+    this.balance = "$0";
+    this.picture = "http://placehold.it/32x32";
+    this.age = 18;
+    this.first_name = "";
+    this.last_name = "";
+    this.company = "";
+    this.email = "";
+    this.phone = "";
+    this.address = "";
+    this.about = "";
+    this.registered = "";
+    this.greeting = "";
+    this.favoriteFruit = "strawberry";
+  }
+}
